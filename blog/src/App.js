d0ebfacf61c2e1664d840128d03f56b1d8c01b80
an2434 <jinhyung2434@naver.com>
2022-07-04 14:05:39 +0900

    state 값 변경& array 오름차순

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,28 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import{useState} from 'react';
+import { useState } from 'react';
 function App() {
   let post = '강남 우동 맛집';
-useState();
+  let [글제목, 제목변경] = useState(['임술농민봉기', '병인양요', '임오군란']);
+  let [like, c] = useState(0);
+
+
   return (
     <div className="App">
       <div className="black-nav">
+        <h4>reactblog</h4>
+      </div>
+      <button onClick={() => {
+        let copy= [...글제목];
+        copy.sort();
+        제목변경(copy);
+      
+      }}>글수정</button>
 
-        <h4>
-          블로그임
+      <div className="list">
+        <h4>{글제목[0]}<span onClick={()=>c(like + 1)}> 🥰</span>{like}</h4>
+        <p>6월 25일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>6월 25일 발행</p>
+      </div>
+      <div className="list">
+
+        <h4>{글제목[2]}
+
+
         </h4>
 
-        {/* <h4 style={{color :'red', fontSize :'16px'}} id = {post}>블로그임</h4> */}
-      </div>
-      <div className ="list">
-        <h4>글제목</h4>
-        <p>6월 25일 발행</p>
         <p>6월 25일 발행</p>
 
       </div>
-      {/* <h4>{post}</h4> */}
-
     </div>
+
+
   );
 }
 
