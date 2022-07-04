4dc9557c73fdeca2160c6e72d4bdf8506ec32a26
an2434 <jinhyung2434@naver.com>
2022-07-04 09:56:48 +0900

    .

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,7 +7,7 @@ useState();
   return (
     <div className="App">
       <div className="black-nav">
-<<<<<<< Updated upstream
+
         <h4>
           블로그임
         </h4>
@@ -21,14 +21,7 @@ useState();
 
       </div>
       {/* <h4>{post}</h4> */}
-=======
-        <h4 >블로그임</h4>
-      </div>
-      <div className="list">
-        <h4>글제목</h4>
-        <p>2월 17일 발행</p>
-      </div>
->>>>>>> Stashed changes
+
     </div>
   );
 }
