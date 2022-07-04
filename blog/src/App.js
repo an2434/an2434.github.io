d98d713a33b14c18d2681e79129a4dfb3209891c
an2434 <jinhyung2434@naver.com>
2022-07-04 09:55:18 +0900

    .

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import{useState} from 'react';
 function App() {
   let post = '강남 우동 맛집';
-  
+useState();
   return (
     <div className="App">
       <div className="black-nav">
+<<<<<<< Updated upstream
         <h4>
           블로그임
         </h4>
@@ -20,6 +21,14 @@ function App() {
 
       </div>
       {/* <h4>{post}</h4> */}
+=======
+        <h4 >블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
