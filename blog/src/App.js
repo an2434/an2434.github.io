2fbc90ed96818fb648188c11c01ac9655710961e
an2434 <jinhyung2434@naver.com>
2022-07-18 21:16:01 +0900

    Update App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,6 +10,7 @@ function App() {
     "띄얼키",
     "ㅁㄴㅇ",
   ]);
+  let [key, setkey] = ([]);
   let [like, setlike] = useState([0, 0, 0, 0, 0]);
   let [drop, setdrop] = useState();
   let [title, settitle] = useState(1);
@@ -19,13 +20,13 @@ function App() {
   function save(e) {
     제목변경(글제목.concat(input));
     setlike(like.concat(0));
-    console.log(input, 글제목);
+
 
     e.stopPropagation();
     setinput("");
   }
   function deletekey(e) {
-    console.log(1);
+    console.log(key);
     e.stopPropagation();
   }
 
@@ -47,11 +48,12 @@ function App() {
 
       {글제목.map(function (a, i) {
         return (
-          <div className="list" key={i}>
+          <div className="list" key={setkey(key.concat(i))}>
             <h4
               onClick={() => {
                 setmodal(true);
                 settitle(i);
+               
               }}
             >
               {글제목[i]}
